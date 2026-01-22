a184535f96af3a2ba52c14c1d326b26d723e123a
Monika 1588 <vermamonika3733@gmail.com>
2026-01-22 07:39:45 +0530

    commit

--- Backend/server.js
+++ Backend/server.js
@@ -1,20 +1,50 @@
+// const express = require("express");
+// const cors = require("cors");
+// const dotenv = require("dotenv");
+// const connectDB = require("./config/db");
+
+// // Allow frontend to access backend
+// app.use(cors({
+//   origin: "https://portfolio-1-nfdx.onrender.com",
+//   credentials: true
+// }));
+
+// dotenv.config();
+// connectDB();
+
+// const app = express();
+
+// app.use(cors());
+// app.use(express.json());
+
+// app.use("/api/projects", require("./routes/projectRoutes"));
+// app.use("/api/feedback", require("./routes/feedbackRoutes"));
+// app.use("/api/contact", require("./routes/contactRoutes"));
+
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () =>
+//   console.log(`Backend server running on port ${PORT}`)
+// );
+
+
+
+
+
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
-import cors from "cors";
-// Allow frontend to access backend
-app.use(cors({
-  origin: "https://portfolio-1-nfdx.onrender.com",
-  credentials: true
-}));
 
 dotenv.config();
+
 connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "https://portfolio-1-nfdx.onrender.com",
+  credentials: true
+}));
 app.use(express.json());
 
 app.use("/api/projects", require("./routes/projectRoutes"));
